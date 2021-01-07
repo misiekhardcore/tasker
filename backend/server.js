d97055142bf3af7026d8e4c81c48e4d7e3cbe422
misiekhardcore <misiekhardcore@gmail.com>
2021-01-07 12:15:49 +0100

    set up apollo server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-const {ApolloServer} = require('apollo-server')
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//MongoDB
+const MONGODB = process.env.MONGODB;
+
+//GRAPHQL
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+//Apollo server
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+//Connect to database and start server
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    return server.listen({ port: process.env.PORT || 5000 });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
