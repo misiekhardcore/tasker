98ac7b07f4e296304d756d5f164a8916f66d47a3
misiekhardcore <misiekhardcore@gmail.com>
2021-01-07 12:15:12 +0100

    userResolver messages

---
+++ backend/messages.js
@@ -0,0 +1,13 @@
+module.exports = {
+  USERNAME_INPUT_EMPTY: "Username can not be empty",
+  PASSWORD_INPUT_EMPTY: "Password can not be empty",
+  PASSWORD_INPUT_NOTMATCH: "Passwords not match",
+  EMAIL_INPUT_EMPTY: "Email can not be empty",
+  EMAIL_INPUT_INVALID: "Enter valid email adress",
+  USER_VALIDATION_ERROR: "Validation error",
+  USER_NOT_FOUND: "User not found",
+  USER_WRONG_CREDENTIALS: "Wrong credentials",
+  USERNAME_ALREADY_EXISTS: "Username already exists",
+  EMAIL_ALREADY_EXISTS: "Email already exists",
+  REGISTER_KEY_NOT_VALID: "Register key not valid",
+};
