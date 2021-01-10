dbdcd1bfa5a77620ac7c6781bc217993926915de
misiekhardcore <misiekhardcore@gmail.com>
2021-01-10 20:34:42 +0100

    refactor/bugfix

--- backend/graphql/utils/authCheck.js
+++ backend/graphql/utils/authCheck.js
@@ -6,27 +6,23 @@ const { AUTHORIZATION_ERROR } = require("../../messages");
 module.exports = (context) => {
   //get header from request context
   const authHeader = context.req.headers.authorization;
+  
 
   const errors = {};
-  let user = null;
 
   if (!authHeader) {
     errors.authorization = AUTHORIZATION_ERROR;
+    throw new AuthenticationError(AUTHORIZATION_ERROR, errors);
   }
 
   const token = authHeader.split("Bearer ")[1];
-
-  if (token) {
-    try {
-      user = jwt.verify(token, JWT_SECRET);
-    } catch (error) {
-      errors.authorization = AUTHORIZATION_ERROR;
-    }
-  }
   
-  return {
-    user: user ? user : null,
-    errors,
-    valid: !Object.keys(errors).length,
-  };
+  if (!token) {
+    errors.authorization = AUTHORIZATION_ERROR;
+    throw new AuthenticationError(AUTHORIZATION_ERROR, errors);
+  }
+
+  const user = jwt.verify(token, JWT_SECRET);
+
+  return user;
 };
