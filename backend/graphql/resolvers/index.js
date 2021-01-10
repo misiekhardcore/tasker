7b283ec2fc175a2270aa0e562f263cfff7bfc4b1
misiekhardcore <misiekhardcore@gmail.com>
2021-01-10 20:32:35 +0100

    add team resolver

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,7 +1,13 @@
 const tableResolver = require("./tableResolver");
 const taskResolver = require("./taskResolver");
 const userResolver = require("./userResolver");
+const teamResolver = require("./teamResolver");
 
 const { merge } = require("lodash");
 
-module.exports = merge(userResolver, taskResolver, tableResolver);
+module.exports = merge(
+  userResolver,
+  taskResolver,
+  tableResolver,
+  teamResolver
+);
