ed0c72abdd049fc394523c97b2a59a2f759f66a8
misiekhardcore <misiekhardcore@gmail.com>
2021-01-10 20:35:09 +0100

    add new validators

--- backend/graphql/utils/validators.js
+++ backend/graphql/utils/validators.js
@@ -1,10 +1,20 @@
 const {
+  AuthenticationError,
+  UserInputError,
+} = require("apollo-server");
+
+const {
   USERNAME_INPUT_EMPTY,
   PASSWORD_INPUT_EMPTY,
   PASSWORD_INPUT_NOTMATCH,
+  USER_WRONG_CREDENTIALS,
   EMAIL_INPUT_EMPTY,
   EMAIL_INPUT_INVALID,
 } = require("../../messages");
+
+const bcrypt = require("bcrypt");
+const User = require("../../models/User");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports.validateRegisterInput = (
   username,
@@ -43,10 +53,11 @@ module.exports.validateRegisterInput = (
     errors.confirmPassword = PASSWORD_INPUT_NOTMATCH;
   }
 
-  return {
-    errors,
-    valid: !Object.keys(errors).length,
-  };
+  if (Object.keys(errors).length > 0) {
+    throw new UserInputError(USER_VALIDATION_ERROR, errors);
+  }
+
+  return;
 };
 
 module.exports.validateLoginInput = (username, password) => {
@@ -63,8 +74,51 @@ module.exports.validateLoginInput = (username, password) => {
     errors.password = PASSWORD_INPUT_EMPTY;
   }
 
-  return {
-    errors,
-    valid: !Object.keys(errors).lenght,
-  };
+  if (Object.keys(errors).length > 0) {
+    throw new UserInputError(USER_VALIDATION_ERROR, errors);
+  }
+
+  return;
+};
+
+module.exports.checkPassword = async (password, hash) => {
+  //collect errors
+  const errors = {};
+
+  //check password
+  const check = await bcrypt.compare(password, hash);
+  if (!check) {
+    errors.general = USER_WRONG_CREDENTIALS;
+    throw new UserInputError(USER_WRONG_CREDENTIALS, errors);
+  }
+  return;
+};
+
+module.exports.checkUser = async (
+  searchObject,
+  errorType,
+  errorMessage,
+  exists = true
+) => {
+  //collect errors
+  const errors = {};
+
+  //check user
+  const user = await User.findOne(searchObject);
+  const state = user ? true : false;
+  if (state != exists) {
+    errors[errorType] = errorMessage;
+    throw new UserInputError(errorMessage, errors);
+  }
+
+  return user;
+};
+
+module.exports.checkId = (id) => {
+  const errors = {};
+
+  if (!ObjectId.isValid(tableId)) {
+    errors.general = NOT_VALID_ID;
+    throw new UserInputError(NOT_VALID_ID, errors);
+  }
 };
