8d25063602637f67e89d4674f48cf4235ff922e9
misiekhardcore <misiekhardcore@gmail.com>
2021-01-12 20:53:38 +0100

    bugfix

--- backend/graphql/utils/validators.js
+++ backend/graphql/utils/validators.js
@@ -8,6 +8,7 @@ const {
   PASSWORD_INPUT_EMPTY,
   PASSWORD_INPUT_NOTMATCH,
   USER_WRONG_CREDENTIALS,
+  USER_VALIDATION_ERROR,
   EMAIL_INPUT_EMPTY,
   EMAIL_INPUT_INVALID,
   NOT_VALID_ID,
@@ -55,19 +56,19 @@ module.exports.validateRegisterInput = (
   }
 
   if (Object.keys(errors).length > 0) {
-    throw new UserInputError(USER_VALIDATION_ERROR, errors);
+    throw new UserInputError(USER_VALIDATION_ERROR, { errors });
   }
 
   return;
 };
 
-module.exports.validateLoginInput = (username, password) => {
+module.exports.validateLoginInput = (email, password) => {
   //collect errors
   const errors = {};
 
   //username empty
-  if (username.trim() === "") {
-    errors.username = USERNAME_INPUT_EMPTY;
+  if (email.trim() === "") {
+    errors.email = EMAIL_INPUT_EMPTY;
   }
 
   //password empty
@@ -76,7 +77,7 @@ module.exports.validateLoginInput = (username, password) => {
   }
 
   if (Object.keys(errors).length > 0) {
-    throw new UserInputError(USER_VALIDATION_ERROR, errors);
+    throw new UserInputError(USER_VALIDATION_ERROR, { errors });
   }
 
   return;
@@ -90,7 +91,7 @@ module.exports.checkPassword = async (password, hash) => {
   const check = await bcrypt.compare(password, hash);
   if (!check) {
     errors.general = USER_WRONG_CREDENTIALS;
-    throw new UserInputError(USER_WRONG_CREDENTIALS, errors);
+    throw new UserInputError(USER_WRONG_CREDENTIALS, { errors });
   }
   return;
 };
@@ -109,7 +110,7 @@ module.exports.checkUser = async (
   const state = user ? true : false;
   if (state != exists) {
     errors[errorType] = errorMessage;
-    throw new UserInputError(errorMessage, errors);
+    throw new UserInputError(errorMessage, { errors });
   }
 
   return user;
@@ -120,6 +121,6 @@ module.exports.checkId = (id) => {
 
   if (id && !ObjectId.isValid(id)) {
     errors.general = NOT_VALID_ID;
-    throw new UserInputError(NOT_VALID_ID, errors);
+    throw new UserInputError(NOT_VALID_ID, { errors });
   }
 };
