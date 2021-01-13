2875172a587bd15c4e171a23c926025f52a4cb53
misiekhardcore <misiekhardcore@gmail.com>
2021-01-13 21:16:46 +0100

    add navbar

---
+++ frontend/src/components/MenuBar.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
+import "./MenuBar.scss";
+
+const MenuBar = () => {
+  const context = useContext(AuthContext);
+
+  return (
+    <div className="menu__container">
+      <Link to='/'><h1 className="menu__logo">Tasker</h1></Link>
+      <nav className="nav">
+        <ul className="nav__links">
+          <li className="nav__link" onClick={context.logout}>
+            Logout
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MenuBar;
