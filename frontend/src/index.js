ffb773b1b423797b4559dd57079cd16ad1837d01
misiekhardcore <misiekhardcore@gmail.com>
2021-01-14 14:42:35 +0100

    apollo devtools true

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,6 +31,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(link),
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 ReactDOM.render(
