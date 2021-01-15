4ac480490541596ee8bfe112cf3f5ccb8d7b69c7
misiekhardcore <misiekhardcore@gmail.com>
2021-01-15 13:41:58 +0100

    move queries to separate folder

---
+++ frontend/src/queries/index.js
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+export const GET_FOLDERS = gql`
+  query getTables($parent: ID) {
+    getTables(parent: $parent) {
+      id
+      name
+      description
+      creator {
+        username
+      }
+      parent {
+        id
+      }
+      updatedAt
+    }
+  }
+`;
+
+export const GET_TASKS = gql`
+  query getTasks($parent: ID!) {
+    getTasks(parent: $parent) {
+      id
+      name
+      description
+      creator {
+        username
+      }
+      parent {
+        id
+      }
+      updatedAt
+    }
+  }
+`;
+
+export const ADD_FOLDER = gql`
+  mutation createTable($name: String!, $description: String, $parent: ID) {
+    createTable(name: $name, description: $description, parent: $parent) {
+      id
+      name
+      description
+      creator {
+        username
+      }
+      parent {
+        id
+      }
+      updatedAt
+    }
+  }
+`;
+
+export const ADD_TASK = gql`
+  mutation createTask($parent: ID!, $name: String!) {
+    createTask(parent: $parent, name: $name) {
+      id
+      name
+      description
+      creator {
+        username
+      }
+      parent {
+        id
+      }
+      updatedAt
+    }
+  }
+`;
