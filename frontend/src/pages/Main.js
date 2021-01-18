df7bf1e16b3e4a3a614ef6ae5faf9aa3414f2c01
misiekhardcore <misiekhardcore@gmail.com>
2021-01-18 13:57:00 +0100

    add TableModify to render

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import FoldersList from "../components/FoldersList";
 import MenuBar from "../components/MenuBar";
+import TableModify from "../components/TableModify";
 
 const Main = () => {
   const [column2, setColumn2] = useState([]);
   const [back, setBack] = useState([]);
+  const [folder, setFolder] = useState();
 
   return (
     <>
       <MenuBar />
       <div className="main__container">
         <div className="column1">
-          <FoldersList setData={setColumn2} />
+          <FoldersList setData={setColumn2} setFolder={setFolder} />
         </div>
         <div className="column2">
           {column2.length > 0 && (
@@ -20,10 +22,13 @@ const Main = () => {
               setData={setColumn2}
               back={back}
               setBack={setBack}
+              setFolder={setFolder}
             />
           )}
         </div>
-        <div className="column3"></div>
+        <div className="column3">
+          {folder && <TableModify folder={folder} setFolder={setFolder} />}
+        </div>
       </div>
     </>
   );
