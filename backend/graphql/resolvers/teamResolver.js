0d118835447d79814b86070b68322fb3b2436776
misiekhardcore <misiekhardcore@gmail.com>
2021-01-24 15:30:19 +0100

    create basic team resolver

--- backend/graphql/resolvers/teamResolver.js
+++ backend/graphql/resolvers/teamResolver.js
@@ -1,9 +1,16 @@
 const Team = require("../../models/Team");
 const User = require("../../models/User");
 const authCheck = require("../utils/authCheck");
+const { checkId } = require("../utils/validators");
 
 module.exports = {
   Query: {
+    getTeam: async (_, { teamId }, context) => {
+      const { id } = authCheck(context);
 
+      checkId(teamId);
+      
+      return await Team.findById(teamId);
+    },
   },
 };
