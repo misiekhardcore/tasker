aae63a4fc7ad57c1948e0f2fc4b33880c5616bba
misiekhardcore <misiekhardcore@gmail.com>
2021-01-26 08:00:18 +0100

    add handleLogout function to clear cache

--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -1,17 +1,29 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
+import { ListContext } from "../context/list";
 import "./MenuBar.scss";
 
 const MenuBar = () => {
-  const context = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
+  const { setTask, setBack, setColumn2, setFolder } = useContext(ListContext);
+
+  function handleLogout() {
+    setBack([]);
+    setColumn2([]);
+    setFolder();
+    setTask();
+    logout();
+  }
 
   return (
     <div className="menu__container">
-      <Link to='/' onClick={() => window.location.reload()}><h1 className="menu__logo">Tasker</h1></Link>
+      <Link to="/" onClick={() => window.location.reload()}>
+        <h1 className="menu__logo">Tasker</h1>
+      </Link>
       <nav className="nav">
         <ul className="nav__links">
-          <li className="nav__link" onClick={context.logout}>
+          <li className="nav__link" onClick={handleLogout}>
             Logout
           </li>
         </ul>
