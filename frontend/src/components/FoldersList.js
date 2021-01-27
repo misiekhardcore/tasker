cbc3a2d98771b06234cd474c008d96374813af4c
misiekhardcore <misiekhardcore@gmail.com>
2021-01-27 19:55:41 +0100

    refactor to manage only folder list

--- frontend/src/components/FoldersList.js
+++ frontend/src/components/FoldersList.js
@@ -1,253 +1,79 @@
-import { gql, useMutation, useQuery } from "@apollo/client";
-import React, { useContext, useState } from "react";
-import { IoMdAdd } from "react-icons/io";
-import { BiArrowBack } from "react-icons/bi";
-import { AiFillDelete, AiFillFolder, AiFillSchedule } from "react-icons/ai";
-import {
-  ADD_FOLDER,
-  ADD_TASK,
-  DELETE_FOLDER,
-  DELETE_TASK,
-  GET_FOLDERS,
-  GET_TASKS,
-} from "../queries";
-import { Button, Form, Input } from "./styled";
+import { useMutation, useQuery } from "@apollo/client";
+import React, { useContext } from "react";
+import { AiFillDelete, AiFillFolder } from "react-icons/ai";
+import { DELETE_FOLDER, GET_FOLDERS } from "../queries";
+import { Button, ListItem, UnorderedList } from "./styled";
 import { ListContext } from "../context/list";
-import Errors from "./Errors";
 
-const FoldersList = ({ subList }) => {
-  const { back, column2, setColumn2, setTask, setFolder, setBack } = useContext(
-    ListContext
-  );
+const FoldersList = ({ parent }) => {
+  const {
+    back,
+    column2,
+    setColumn2,
+    setTask,
+    setFolder,
+    setBack,
+  } = useContext(ListContext);
 
-  let back2 = [...back];
-  let parents2 = [...column2];
-  const parent = subList
-    ? parents2[column2.length - 1] || undefined
-    : undefined;
-  const prev = parents2[column2.length - 2] || undefined;
-
-  const [tableName, setTableName] = useState("");
-  const [taskName, setTaskName] = useState("");
-  const [errors, setErrors] = useState({});
-
-  const { data } = useQuery(GET_FOLDERS, {
+  const { loading, error, data } = useQuery(GET_FOLDERS, {
     variables: {
       parent,
     },
-  });
-
-  const { data: data2 } = useQuery(GET_TASKS, {
-    variables: {
-      parent,
-    },
-  });
-
-  const [addFolder] = useMutation(ADD_FOLDER, {
-    variables: {
-      name: tableName,
-      parent,
-    },
-    update(cache, { data: { createTable } }) {
-      cache.modify({
-        fields: {
-          getTables(existingTables = []) {
-            const newTableRef = cache.writeFragment({
-              data: createTable,
-              fragment: gql`
-                fragment NewTable on Tables {
-                  id
-                  type
-                }
-              `,
-            });
-            return [...existingTables, newTableRef];
-          },
-        },
-      });
-    },
-    onCompleted() {
-      setErrors({});
-    },
-    onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors);
-    },
-  });
-
-  const [addTask] = useMutation(ADD_TASK, {
-    variables: {
-      name: taskName,
-      parent,
-    },
-    update() {},
-    onCompleted() {
-      setErrors({});
-    },
-    onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors);
-    },
-    refetchQueries: [
-      { query: GET_TASKS, variables: { parent } },
-      { query: GET_FOLDERS, variables: { parent } },
-    ],
   });
 
   const [deleteFolder] = useMutation(DELETE_FOLDER, {
-    onCompleted() {
-      setErrors({});
-    },
+    onCompleted() {},
     onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+      throw new Error(err);
     },
-    refetchQueries: [
-      { query: GET_TASKS, variables: { parent } },
-      { query: GET_FOLDERS, variables: { parent } },
-    ],
+    refetchQueries: [{ query: GET_FOLDERS, variables: { parent } }],
   });
 
-  const [deleteTask] = useMutation(DELETE_TASK, {
-    onCompleted() {
-      setErrors({});
-    },
-    onError(err) {
-      setErrors(err.graphQLErrors[0].extensions.exception.errors);
-    },
-    refetchQueries: [
-      { query: GET_TASKS, variables: { parent } },
-      { query: GET_FOLDERS, variables: { parent } },
-    ],
-  });
-
-  function handleParent(id, name) {
-    if (subList) {
-      parents2.push(id);
-      back2.push(name);
+  function handleParent(nextId, backName) {
+    if (parent) {
+      setColumn2(column2.push(nextId));
+      setBack(back.push(backName));
     } else {
-      parents2 = [id];
-      back2 = [name];
+      setColumn2([nextId]);
+      setBack([backName]);
     }
-    setColumn2(parents2);
-    setBack(back2);
-    setFolder(id);
+    setFolder(nextId);
     setTask();
-  }
-
-  function handleBack() {
-    parents2.pop();
-    back2.pop();
-    setColumn2(parents2);
-    setBack(back2);
-  }
-
-  function handleAddFolder(e) {
-    e.preventDefault();
-    addFolder();
-    setTableName("");
   }
 
   function handleDeleteFolder(id) {
     deleteFolder({
       variables: { parent: id },
     });
-    setColumn2(parents2);
-    setBack(back2);
     setFolder();
   }
 
-  function handleAddTask(e) {
-    e.preventDefault();
-    addTask();
-    setTaskName("");
-  }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( {JSON.stringify(error, null, 2)}</p>;
 
-  function handleDeleteTask(id) {
-    deleteTask({
-      variables: { taskId: id },
-    });
-    setTask();
-  }
+  const { getTables } = data;
 
   return (
-    <ul className="list__items">
-      {subList && prev && (
-        <li key="1" className="list__item" onClick={handleBack}>
-          <BiArrowBack />
-          <p>{back[back.length - 1] || ""}</p>
-        </li>
-      )}
-      <li className="list__item" key="2">
-        <Form flex onSubmit={handleAddFolder}>
-          <Input
-            name="name"
-            type="text"
-            value={tableName}
-            onChange={(e) => setTableName(e.target.value)}
-            placeholder="Add new table..."
-          />
-          <Button type="submit">
-            <IoMdAdd />
+    <UnorderedList>
+      {getTables.map((table) => (
+        <ListItem table data-tooltip={table.name} key={table.id}>
+          <AiFillFolder />
+          <p
+            onClick={() => {
+              handleParent(table.id, table.name);
+            }}
+          >
+            {table.name}
+          </p>
+          <Button
+            transparent
+            onClick={() => handleDeleteFolder(table.id)}
+          >
+            <AiFillDelete />
           </Button>
-        </Form>
-      </li>
-      {subList && parent && (
-        <li className="list__item" key="3">
-          <Form flex onSubmit={handleAddTask}>
-            <Input
-              name="name"
-              type="text"
-              value={taskName}
-              onChange={(e) => setTaskName(e.target.value)}
-              placeholder="Add new task..."
-            />
-            <Button type="submit">
-              <IoMdAdd />
-            </Button>
-          </Form>
-        </li>
-      )}
-      <Errors errors={errors} />
-      {data &&
-        data.getTables.map((table) => (
-          <li
-            className="list__item table"
-            data-tooltip={table.name}
-            key={table.id}
-          >
-            <AiFillFolder />
-            <p
-              onClick={() => {
-                handleParent(table.id, table.name);
-              }}
-            >
-              {table.name}
-            </p>
-            <Button onClick={() => handleDeleteFolder(table.id)}>
-              <AiFillDelete />
-            </Button>
-          </li>
-        ))}
-      {data2 &&
-        data2.getTasks.map((task) => (
-          <li
-            className="list__item task"
-            data-tooltip={task.name}
-            key={task.id}
-          >
-            <AiFillSchedule />
-            <p
-              onClick={() => {
-                setTask(task.id);
-                setFolder();
-              }}
-            >
-              {task.name}
-            </p>
-            <Button onClick={() => handleDeleteTask(task.id)}>
-              <AiFillDelete />
-            </Button>
-          </li>
-        ))}
-    </ul>
+        </ListItem>
+      ))}
+    </UnorderedList>
   );
 };
 
