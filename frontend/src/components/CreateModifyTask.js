2aa3960004173f5d738fdfa2f32940cc6ceefe88
misiekhardcore <misiekhardcore@gmail.com>
2021-01-28 19:30:26 +0100

    change textarea to ckeditor

--- frontend/src/components/CreateModifyTask.js
+++ frontend/src/components/CreateModifyTask.js
@@ -12,44 +12,56 @@ import {
   FormGroup,
   Input,
   Label,
-  Textarea,
 } from "./styled";
 import { ListContext } from "../context/list";
 import Errors from "./Errors";
+import Editor from "./Editor";
 
 const CreateModifyTask = () => {
   const [task2, setTask2] = useState({});
   const { task, setTask } = useContext(ListContext);
+  const [errors, setErrors] = useState({});
+  const [desc, setDesc] = useState("");
 
   //get task info
-  useQuery(GET_TASK, {
+  const { loading, error } = useQuery(GET_TASK, {
     variables: { taskId: task },
     onCompleted({ getTask }) {
       setTask2(getTask);
+      setDesc(getTask.description);
+      setErrors({});
     },
     onError(err) {
       setErrors(err.graphQLErrors[0].extensions.exception.errors);
     },
   });
 
-  const { id, name, description, parent, creator, status, createdAt } = task2;
+  const {
+    id,
+    name,
+    description,
+    parent,
+    creator,
+    status,
+    createdAt,
+  } = task2;
   const [state, setState] = useState({
     name: "",
     description: "",
     status: "New",
   });
-  const [errors, setErrors] = useState({});
 
   const [update] = useMutation(UPDATE_TASK, {
     variables: {
       taskId: id,
       name: state.name,
-      description: state.description,
+      description: desc,
       parent: parent && parent.id,
       status: state.status,
     },
     onCompleted({ updateTask }) {
       setTask2(updateTask);
+      setDesc(updateTask.description);
       setErrors({});
     },
     onError(err) {
@@ -60,6 +72,7 @@ const CreateModifyTask = () => {
 
   function handleChange(e) {
     setState({ ...state, [e.target.name]: e.target.value });
+    console.log(e.target.name);
   }
 
   useEffect(() => {
@@ -75,108 +88,98 @@ const CreateModifyTask = () => {
     update();
   }
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( {JSON.stringify(error, null, 2)}</p>;
+
   return (
-    <>
-      {task2 && (
-        <div className="table-details">
-          <ButtonClose onClick={() => setTask()}>
-            <AiOutlineCloseCircle />
-          </ButtonClose>
-          <div className="folder__info">
-            <div className="icon">
-              <AiFillSchedule className="icon" />
-            </div>
-
-            <span className="folder__date">
-              {(createdAt &&
-                moment(+createdAt).format("YYYY-MM-DD, dddd hh:mm")) ||
-                ""}
-            </span>
-            <h2 className="folder__name">
-              {parent && (
-                <>
-                  {parent.name}
-                  {" > "}
-                </>
-              )}
-              <span>{name}</span>
-            </h2>
-            <p className="folder__creator">
-              Created by:
-              <span
-                className="avatar"
-                style={{
-                  backgroundColor: `#${creator && creator.avatar}`,
-                }}
-              ></span>
-              <span>{(creator && creator.username) || "no creator"}</span>
-            </p>
-
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Label htmlFor="name" className="form__label">
-                  Table name:
-                </Label>
-                <Input
-                  value={state.name}
-                  onChange={handleChange}
-                  name="name"
-                  type="text"
-                  className={errors.name || errors.general ? "error" : ""}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label htmlFor="description">Table descrition:</Label>
-                <Textarea
-                  style={{ resize: "none" }}
-                  value={state.description}
-                  onChange={handleChange}
-                  name="description"
-                  type="text"
-                  className={
-                    errors.description || errors.general ? "error" : ""
-                  }
-                />
-              </FormGroup>
-              <FormGroup>
-                <select
-                  name="status"
-                  value={state.status}
-                  onChange={handleChange}
-                  style={{
-                    backgroundColor:
-                      state.status === "New"
-                        ? "red"
-                        : state.status === "In progress"
-                        ? "yellow"
-                        : "green",
-                  }}
-                >
-                  <option
-                    style={{ backgroundColor: "red" }}
-                    value="New"
-                  ></option>
-                  <option
-                    style={{ backgroundColor: "yellow" }}
-                    value="In progress"
-                  ></option>
-                  <option
-                    style={{ backgroundColor: "green" }}
-                    value="Finished"
-                  ></option>
-                </select>
-                {status}
-              </FormGroup>
-              <Errors errors={errors} />
-              <Button type="submit" block>
-                Save
-              </Button>
-            </Form>
-          </div>
-          {id && <Comments taskId={id} />}
+    <div className="table-details">
+      <ButtonClose onClick={() => setTask()}>
+        <AiOutlineCloseCircle />
+      </ButtonClose>
+      <div className="folder__info">
+        <div className="icon">
+          <AiFillSchedule className="icon" />
         </div>
-      )}
-    </>
+
+        <span className="folder__date">
+          {(createdAt &&
+            moment(+createdAt).format("YYYY-MM-DD, dddd hh:mm")) ||
+            ""}
+        </span>
+        <h2 className="folder__name">
+          {parent && (
+            <>
+              {parent.name}
+              {" > "}
+            </>
+          )}
+          <span>{name}</span>
+        </h2>
+        <p className="folder__creator">
+          Created by:
+          <span
+            className="avatar"
+            style={{
+              backgroundColor: `#${creator && creator.avatar}`,
+            }}
+          ></span>
+          <span>{(creator && creator.username) || "no creator"}</span>
+        </p>
+
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label htmlFor="name" className="form__label">
+              Table name:
+            </Label>
+            <Input
+              value={state.name}
+              onChange={handleChange}
+              name="name"
+              type="text"
+              className={errors.name || errors.general ? "error" : ""}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="description">Table descrition:</Label>
+            <Editor data={desc} state={setDesc} />
+          </FormGroup>
+          <FormGroup>
+            <select
+              name="status"
+              value={state.status}
+              onChange={handleChange}
+              style={{
+                backgroundColor:
+                  state.status === "New"
+                    ? "red"
+                    : state.status === "In progress"
+                    ? "yellow"
+                    : "green",
+              }}
+            >
+              <option
+                style={{ backgroundColor: "red" }}
+                value="New"
+              ></option>
+              <option
+                style={{ backgroundColor: "yellow" }}
+                value="In progress"
+              ></option>
+              <option
+                style={{ backgroundColor: "green" }}
+                value="Finished"
+              ></option>
+            </select>
+            {status}
+          </FormGroup>
+          <Errors errors={errors} />
+          <Button type="submit" block>
+            Save
+          </Button>
+        </Form>
+      </div>
+      {id && <Comments taskId={id} />}
+    </div>
   );
 };
 
