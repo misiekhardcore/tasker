7cb302fe6710a6c8443b4e5999f77c273e86f69c
misiekhardcore <misiekhardcore@gmail.com>
2021-01-31 21:40:50 +0100

    add styled components

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -6,9 +6,39 @@ import TasksList from "./TasksList";
 import FoldersList from "./FoldersList";
 import FolderAdd from "./FolderAdd";
 import TaskAdd from "./TaskAdd";
+import styled from "styled-components";
+
+const BackButton = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  padding: 0.3rem 0.5rem;
+  border: none;
+  border-radius: 0.25rem;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    cursor: pointer;
+    background-color: lightgray;
+    transform: scale(1.05);
+  }
+
+  p {
+    flex-grow: 1;
+    margin-left: 0.5rem;
+    text-align: left;
+  }
+
+  svg {
+    font-size: 1.2rem;
+  }
+`;
 
 const List = ({ subList }) => {
-  const { back, column2, setColumn2, setBack } = useContext(ListContext);
+  const { back, column2, setColumn2, setBack } = useContext(
+    ListContext
+  );
 
   let back2 = [...back];
   let parents2 = [...column2];
@@ -28,17 +58,17 @@ const List = ({ subList }) => {
 
   return (
     <>
-      <ul className="list__items">
-        {subList && prev && (
-          <li key="1" className="list__item" onClick={handleBack}>
-            <BiArrowBack />
-            <p>{back[back.length - 1] || ""}</p>
-          </li>
-        )}
-        <FolderAdd parent={parent} setErrors={setErrors} />
-        {subList && parent && <TaskAdd parent={parent} setErrors={setErrors} />}
-        <Errors errors={errors} />
-      </ul>
+      {subList && prev && (
+        <BackButton key="1" className="list__item" onClick={handleBack}>
+          <BiArrowBack />
+          <p>{back[back.length - 1] || ""}</p>
+        </BackButton>
+      )}
+      <FolderAdd parent={parent} setErrors={setErrors} />
+      {subList && parent && (
+        <TaskAdd parent={parent} setErrors={setErrors} />
+      )}
+      <Errors errors={errors} />
       <FoldersList parent={parent} />
       <TasksList parent={parent} />
     </>
