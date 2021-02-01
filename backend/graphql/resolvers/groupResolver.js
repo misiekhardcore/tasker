68b7b71464f4e8e8e5c00e7f851ffd4f0ceb4b94
misiekhardcore <misiekhardcore@gmail.com>
2021-02-01 20:08:04 +0100

    add createGroup resolver

--- backend/graphql/resolvers/groupResolver.js
+++ backend/graphql/resolvers/groupResolver.js
@@ -1,10 +1,35 @@
+const Group = require("../../models/Group");
+const Table = require("../../models/Table");
+const Team = require("../../models/Team");
+const authCheck = require("../utils/authCheck");
+const { randomChoice } = require("../utils/helpers");
+
 module.exports = {
   Query: {
     getGroups: async (_, { userId }, context) => {},
     getGroup: async (_, { groupId }, context) => {},
   },
   Mutation: {
-    createGroup: async (_, { users, parent }, context) => {},
+    createGroup: async (_, { parent }, context) => {
+      const { id, team } = authCheck(context);
+      try {
+        const { users } = parent
+          ? await Group.findById((await Table.findById(parent)).group)
+          : await Team.findById(team);
+
+        const group = await Group.create({
+          creator: id,
+          avatar: randomChoice(),
+          users,
+        });
+
+        return await Group.findById(group._id)
+          .populate("users")
+          .populate("creator");
+      } catch (error) {
+        console.log(error);
+      }
+    },
     updateGroup: async (_, { users, groupId }, context) => {},
   },
 };
