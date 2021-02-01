a58d208201741597be7b8c3bd289e5036b8a4d84
misiekhardcore <misiekhardcore@gmail.com>
2021-02-01 20:15:07 +0100

    remove parent ref

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -5,8 +5,6 @@ const groupSchema = new Schema(
     creator: { type: Schema.Types.ObjectId, ref: "User" },
     avatar: String,
     users: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    parent: { type: Schema.Types.ObjectId, refPath: "Parent" },
-    Parent: { type: String, enum: ["Table", "Task"] },
   },
 
   {
