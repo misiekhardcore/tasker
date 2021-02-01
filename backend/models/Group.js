77240d3583d3310d921d4165609fbc0647f8ca94
misiekhardcore <misiekhardcore@gmail.com>
2021-02-01 13:14:46 +0100

    modify Group model

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -5,7 +5,10 @@ const groupSchema = new Schema(
     creator: { type: Schema.Types.ObjectId, ref: "User" },
     avatar: String,
     users: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    parent: { type: Schema.Types.ObjectId, refPath: "Parent" },
+    Parent: { type: String, enum: ["Table", "Task"] },
   },
+
   {
     timestamps: true,
   }
