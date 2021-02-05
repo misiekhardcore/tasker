5cb215efeb7ae8f16838c77e264147fc5c5a9459
misiekhardcore <misiekhardcore@gmail.com>
2021-02-05 13:31:47 +0100

    move to styled components

--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -2,13 +2,62 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
 import { ListContext } from "../context/list";
-import "./MenuBar.scss";
+import styled from "styled-components";
+
+const MenuContainer = styled.div`
+  width: 100%;
+  padding: 0.5rem 0.8rem;
+  display: flex;
+  align-items: center;
+  background-color: #aaa;
+  color: white;
+  font-family: "Sofia", cursive;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const Logo = styled(Link)`
+  font-family: inherit;
+  font-style: italic;
+  font-size: 2rem;
+  color: white;
+
+  &:hover {
+    transform: scale(1.05) rotate(5deg);
+  }
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  font-family: inherit;
+
+  ul {
+    font-family: inherit;
+    display: flex;
+    list-style: none;
+    color: $white;
+    font-size: 1.5rem;
+
+    li {
+      font-family: inherit;
+      transition: all 0.2s ease-in-out;
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.05);
+      }
+
+      & + li {
+        margin-left: 1rem;
+      }
+    }
+  }
+`;
 
 const MenuBar = () => {
   const { logout, user } = useContext(AuthContext);
-  const { setTask, setBack, setColumn2, setFolder } = useContext(
-    ListContext
-  );
+  const { setTask, setBack, setColumn2, setFolder } = useContext(ListContext);
 
   function handleLogout() {
     setBack([]);
@@ -19,19 +68,17 @@ const MenuBar = () => {
   }
 
   return (
-    <div className="menu__container">
-      <Link to="/" onClick={() => window.location.reload()}>
-        <h1 className="menu__logo">Tasker</h1>
-      </Link>
-      <nav className="nav">
-        <ul className="nav__links">
-          <li className="nav__link">{user.username}</li>
-          <li className="nav__link" onClick={handleLogout}>
-            Logout
-          </li>
+    <MenuContainer>
+      <Logo to="/" onClick={() => window.location.reload()}>
+        Tasker
+      </Logo>
+      <Nav>
+        <ul>
+          <li>{user.username}</li>
+          <li onClick={handleLogout}>Logout</li>
         </ul>
-      </nav>
-    </div>
+      </Nav>
+    </MenuContainer>
   );
 };
 
