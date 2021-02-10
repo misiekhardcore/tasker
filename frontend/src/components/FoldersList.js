ba6635cda28a075e5c3622b35bf0d80969efd6cd
misiekhardcore <misiekhardcore@gmail.com>
2021-02-10 14:44:49 +0100

    add cache delete handling

--- frontend/src/components/FoldersList.js
+++ frontend/src/components/FoldersList.js
@@ -1,7 +1,15 @@
 import { useMutation, useQuery } from "@apollo/client";
 import React, { useContext } from "react";
 import { AiFillDelete, AiFillFolder } from "react-icons/ai";
-import { DELETE_FOLDER, GET_FOLDERS } from "../queries";
+import {
+  DELETE_FOLDER,
+  GET_COMMENTS,
+  GET_FOLDER,
+  GET_FOLDERS,
+  GET_GROUP,
+  GET_TASK,
+  GET_TASKS,
+} from "../queries";
 import { Button, ListItem, UnorderedList } from "./styled";
 import { ListContext } from "../context/list";
 import Loading from "./Loading";
@@ -19,32 +27,8 @@ const FoldersList = ({ parent }) => {
   });
 
   const [deleteFolder] = useMutation(DELETE_FOLDER, {
-    update(cache) {
-      cache.modify({
-        fields: {
-          getTables(existingTables = [], { DELETE }) {
-            return DELETE;
-          },
-          getGroup(existingGroup = [], { DELETE }) {
-            return { DELETE };
-          },
-          getTable(existinfTable = [], { DELETE }) {
-            return DELETE;
-          },
-          getTasks(existingTasks = [], { DELETE }) {
-            return DELETE;
-          },
-          getTask(existingTask = [], { DELETE }) {
-            return DELETE;
-          },
-          getComments(existingComments = [], { DELETE }) {
-            return DELETE;
-          },
-        },
-      });
-    },
     onError(err) {
-      throw new Error(err);
+      console.log(err);
     },
   });
 
@@ -60,9 +44,121 @@ const FoldersList = ({ parent }) => {
     setTask();
   }
 
-  function handleDeleteFolder(id) {
+  function handleDeleteSubfolders(cache, id) {
+    const a = cache.readQuery({
+      query: GET_FOLDERS,
+      variables: { parent: id },
+    });
+
+    if (a) {
+      handleDeleteSubtasks(cache, id);
+
+      const { getTables } = a;
+
+      getTables.forEach(function (x) {
+        cache.writeQuery({
+          query: GET_FOLDER,
+          data: { getTable: null },
+          variables: { tableId: x.id },
+        });
+        cache.writeQuery({
+          query: GET_GROUP,
+          data: { getGroup: null },
+          variables: { groupId: x.group.id },
+        });
+        handleDeleteSubfolders(cache, x.id);
+      });
+    }
+    cache.writeQuery({
+      query: GET_FOLDERS,
+      data: {
+        getTables: [],
+      },
+      variables: { parent: id },
+    });
+  }
+
+  function handleDeleteSubtasks(cache, id) {
+    const a = cache.readQuery({
+      query: GET_TASKS,
+      variables: { parent: id },
+    });
+
+    if (a) {
+      const { getTasks } = a;
+
+      getTasks.forEach(function (x) {
+        cache.writeQuery({
+          query: GET_TASK,
+          data: { getTask: null },
+          variables: { taskId: x.id },
+        });
+        cache.writeQuery({
+          query: GET_GROUP,
+          data: { getGroup: null },
+          variables: { groupId: x.group.id },
+        });
+        cache.writeQuery({
+          query: GET_COMMENTS,
+          data: { getComments: [] },
+          variables: { parent: x.id },
+        });
+      });
+
+      cache.writeQuery({
+        query: GET_TASKS,
+        data: { getTasks: null },
+        variables: { parent: id },
+      });
+    }
+  }
+
+  function handleDeleteFolder(id, name = "") {
+    const r = window.confirm(`Are you sure you want to delete ${name}?`);
+    if (!r) return;
     deleteFolder({
       variables: { parent: id },
+      update(cache) {
+        try {
+          handleDeleteSubfolders(cache, id);
+          const { getTables } = cache.readQuery({
+            query: GET_FOLDERS,
+            variables: { parent },
+          });
+
+          const { getTable } =
+            cache.readQuery({
+              query: GET_FOLDER,
+              variables: { tableId: id },
+            }) || {};
+          cache.writeQuery({
+            query: GET_FOLDER,
+            data: {
+              getTable: null,
+            },
+            variables: {
+              tableId: id,
+            },
+          });
+
+          if (getTable)
+            cache.writeQuery({
+              query: GET_GROUP,
+              data: { getGroup: null },
+              variables: { groupId: getTable.group.id },
+            });
+
+          cache.writeQuery({
+            query: GET_FOLDERS,
+            data: {
+              getTables: getTables.filter((table) => table.id !== id),
+            },
+            variables: { parent },
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      },
     });
     setFolder();
     if (!parent) {
@@ -89,7 +185,10 @@ const FoldersList = ({ parent }) => {
             >
               {table.name}
             </p>
-            <Button transparent onClick={() => handleDeleteFolder(table.id)}>
+            <Button
+              transparent
+              onClick={() => handleDeleteFolder(table.id, table.name)}
+            >
               <AiFillDelete />
             </Button>
           </ListItem>
